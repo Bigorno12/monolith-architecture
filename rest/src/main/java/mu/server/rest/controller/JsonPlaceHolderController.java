633dadf1ce8f6bdc8ffb69526fe0b131421a1bbd
Pranav <pseew007@icloud.com>
2023-10-09 00:18:39 +0400

    Implement a OpenApi for the restcontroller and creating all the repository

--- rest/src/main/java/mu/server/rest/controller/JsonPlaceHolderController.java
+++ rest/src/main/java/mu/server/rest/controller/JsonPlaceHolderController.java
@@ -1,11 +1,18 @@
 package mu.server.rest.controller;
 
-import mu.server.persistence.entity.Todo;
 import mu.server.persistence.entity.User;
 import mu.server.rest.config.RestTemplateConfig;
+import mu.server.service.dto.CommentDto;
+import mu.server.service.dto.PostDto;
+import mu.server.service.dto.TodoDto;
+import mu.server.service.dto.UserDto;
 import mu.server.service.exception.JsonPlaceHolderException;
+import mu.server.service.jsonplaceholder.CommentJsonPlaceHolder;
+import mu.server.service.jsonplaceholder.PostJsonPlaceHolder;
+import mu.server.service.jsonplaceholder.TodoJsonPlaceHolder;
 import mu.server.service.service.JsonPlaceHolderService;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +21,8 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/v1/jsonplaceholder")
-public class JsonPlaceHolderController {
+@RequestMapping("/api/v1")
+public class JsonPlaceHolderController implements JsonplaceholderApi {
 
     private final JsonPlaceHolderService jsonPlaceHolderService;
     private final RestTemplateConfig restTemplateConfig;
@@ -25,23 +32,58 @@ public class JsonPlaceHolderController {
         this.restTemplateConfig = restTemplateConfig;
     }
 
-    @GetMapping("/users")
-    public void saveAllUsers() {
+    @Override
+    public ResponseEntity<UserDto> retrieveUsers() {
         List<User> users = Optional.ofNullable(restTemplateConfig.restTemplateJsonPlaceHolder()
                         .getForEntity("/users", User[].class)
                         .getBody())
                 .map(Arrays::asList)
                 .orElseThrow(() -> new JsonPlaceHolderException("Failed to get users from jsonplaceholder.typicode.com"));
+
         jsonPlaceHolderService.saveAllUsers(users);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @GetMapping("/todos")
-    public void saveAllTodos() {
-        List<Todo> todos = Optional.ofNullable(restTemplateConfig.restTemplateJsonPlaceHolder()
-                        .getForEntity("/todos", Todo[].class)
+    @Override
+    public ResponseEntity<TodoDto> retrieveTodos() {
+        List<TodoJsonPlaceHolder> todoJsonPlaceHolders = Optional.ofNullable(restTemplateConfig.restTemplateJsonPlaceHolder()
+                        .getForEntity("/todos", TodoJsonPlaceHolder[].class)
                         .getBody())
                 .map(Arrays::asList)
                 .orElseThrow(() -> new JsonPlaceHolderException("Failed to get todos from jsonplaceholder.typicode.com"));
-        jsonPlaceHolderService.saveAllTodos(todos);
+        jsonPlaceHolderService.saveAllTodos(todoJsonPlaceHolders);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
+    @Override
+    public ResponseEntity<PostDto> retrievePosts() {
+
+        List<PostJsonPlaceHolder> postJsonPlaceHolders = Optional.ofNullable(restTemplateConfig.restTemplateJsonPlaceHolder()
+                        .getForEntity("/posts", PostJsonPlaceHolder[].class)
+                        .getBody())
+                .map(Arrays::asList)
+                .orElseThrow(() -> new JsonPlaceHolderException("Failed to get posts from jsonplaceholder.typicode.com"));
+
+        jsonPlaceHolderService.saveAllPosts(postJsonPlaceHolders);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @Override
+    public ResponseEntity<CommentDto> retrieveComments() {
+        List<CommentJsonPlaceHolder> commentJsonPlaceHolders = Optional.ofNullable(restTemplateConfig.restTemplateJsonPlaceHolder()
+                        .getForEntity("/comments", CommentJsonPlaceHolder[].class)
+                        .getBody())
+                .map(Arrays::asList)
+                .orElseThrow(() -> new JsonPlaceHolderException("Failed to get comments from jsonplaceholder.typicode.com"));
+
+        jsonPlaceHolderService.saveAllComments(commentJsonPlaceHolders);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+
+    }
+
+
 }
