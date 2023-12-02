da7a00006fe1d4ea9679a5d481f65f89a58d4d29
Pranav <pseew007@icloud.com>
2023-12-03 01:41:42 +0400

    Upgrade to spring boot 3.2.0 and replace rest template with rest client

--- rest/src/main/java/mu/server/rest/config/RestTemplateConfig.java
+++ rest/src/main/java/mu/server/rest/config/RestTemplateConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -20,6 +21,14 @@ public class RestTemplateConfig {
     public RestTemplate restTemplateJsonPlaceHolder() {
         return restTemplateBuilder
                 .rootUri("https://jsonplaceholder.typicode.com")
+                .defaultHeader("Content-Type", "application/json")
+                .build();
+    }
+
+    @Bean("restClient")
+    public RestClient restClient() {
+        return RestClient.builder()
+                .baseUrl("https://jsonplaceholder.typicode.com")
                 .defaultHeader("Content-Type", "application/json")
                 .build();
     }
