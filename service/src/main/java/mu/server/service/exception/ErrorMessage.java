5d2b16fcdd767b94b88f0ee24b48de11c896996c
Pranav <46899146+Bigorno12@users.noreply.github.com>
2024-04-18 16:08:13 +0400

    Move ErrorMessage.java in the GlobalExceptionHandler class

--- service/src/main/java/mu/server/service/exception/ErrorMessage.java
+++
@@ -1,8 +0,0 @@
-package mu.server.service.exception;
-
-import lombok.Builder;
-
-import java.time.LocalDateTime;
-
-@Builder
-public record ErrorMessage(int statusCode, LocalDateTime timestamp, String message, String description) { }
