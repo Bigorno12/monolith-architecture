b9f3517028d54af856517f5ffbb1ee36075214d7
Pranav <46899146+Bigorno12@users.noreply.github.com>
2024-06-12 16:40:41 +0400

    Add projection using Querydsl, create a todo list in readme

--- service/src/main/java/mu/server/service/mapper/UserMapper.java
+++ service/src/main/java/mu/server/service/mapper/UserMapper.java
@@ -2,11 +2,12 @@ package mu.server.service.mapper;
 
 import mu.server.persistence.entity.User;
 import mu.server.service.dto.UserDto;
+import mu.server.service.mapper.user.UserDtoProjectionMapper;
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
-public interface UserMapper {
+public interface UserMapper extends UserDtoProjectionMapper {
 
     User mapToEntity(UserDto userDto);
 
