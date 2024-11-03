699cff2344944ba89f94084a7861fec4d831c7d7
Pranav <46899146+Bigorno12@users.noreply.github.com>
2024-11-03 21:45:23 +0400

    add dependabot to update dependencies and use CQRS in the Dtos and change name of controller

--- rest/src/main/java/mu/server/rest/controller/UserController.java
+++ rest/src/main/java/mu/server/rest/controller/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,8 +24,11 @@ public class UserController implements UsersApi {
         this.userService = userService;
     }
 
-    @GetMapping("/find-all-user-by-name")
-    public Page<UserDto> findAllUserByName(@RequestParam(name = "pageNum", defaultValue = "0") Integer pageNum, @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize, @RequestParam(name = "name") String name) {
-        return userService.findAllUserByName(PageRequest.of(pageNum, pageSize, Sort.by(Sort.Direction.ASC, "name")), name);
+    @GetMapping("/findUsersByName")
+    public ResponseEntity<Page<UserDto>> findAllUserByName(@RequestParam(name = "pageNum", defaultValue = "0") Integer pageNum, @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize, @RequestParam(name = "name") String name) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(userService.findAllUserByName(PageRequest.of(pageNum, pageSize, Sort.by(Sort.Direction.ASC, "name")), name));
     }
+
 }
