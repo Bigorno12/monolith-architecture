977061c3e3335448903d3170b665b9faa548cd8a
Pranav <46899146+Bigorno12@users.noreply.github.com>
2025-01-15 13:07:52 +0400

    Making blaze persistence to work

--- rest/src/main/java/mu/server/rest/config/ApplicationConfig.java
+++ rest/src/main/java/mu/server/rest/config/ApplicationConfig.java
@@ -3,9 +3,11 @@ package mu.server.rest.config;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
+@EnableJpaAuditing
 @ComponentScan(basePackages = "mu.server")
 @EntityScan(basePackages = "mu.server.persistence.entity")
 @EnableJpaRepositories(basePackages = "mu.server.persistence.repository")
