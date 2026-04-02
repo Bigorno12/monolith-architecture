23ac971b3595464d46472866f7215ca01d04cee5
pranavseewoodharry <pranav.seewoodharry@welldev.io>
2026-04-02 16:19:35 +0400

    feat: add validation error handling in GlobalExceptionHandler

--- rest/src/main/java/mu/server/rest/advice/GlobalExceptionHandler.java
+++ rest/src/main/java/mu/server/rest/advice/GlobalExceptionHandler.java
@@ -2,15 +2,20 @@ package mu.server.rest.advice;
 
 import lombok.Builder;
 import mu.server.service.exception.UsernameExistException;
+import org.jspecify.annotations.Nullable;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -54,6 +59,30 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorMessage, HttpStatus.NOT_FOUND);
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> handleValidation(MethodArgumentNotValidException ex) {
+        Map<String, @Nullable String> errors = ex.getBindingResult().getFieldErrors().stream()
+                .collect(Collectors.toMap(FieldError::getField, fieldError -> {
+                    if (fieldError.getDefaultMessage() != null) {
+                        return fieldError.getDefaultMessage();
+                    }
+                    return "No Default Message";
+                }));
+
+        ValidationError validationError = ValidationError.builder()
+                .statutCode(HttpStatus.BAD_REQUEST.value())
+                .errors(errors)
+                .timestamp(LocalDateTime.now())
+                .message(ex.getCause().getMessage())
+                .build();
+
+        return new ResponseEntity<>(validationError, HttpStatus.BAD_REQUEST);
+    }
+
     @Builder
     public record ErrorMessage(int statusCode, LocalDateTime timestamp, String message, String description) { }
+
+    @Builder
+    public record ValidationError(int statutCode, LocalDateTime timestamp, String message, Map<String, String> errors) {}
 }
