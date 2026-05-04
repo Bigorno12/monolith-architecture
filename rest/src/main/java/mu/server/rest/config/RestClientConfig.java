53f140682f950d902007cbc234cce5a2c85c5860
Pranav <46899146+Bigorno12@users.noreply.github.com>
2026-05-04 15:10:38 +0400

    feat: enhance authentication flow and error handling, update logging configuration (#215)
    Co-authored-by: pranavseewoodharry <pranav.seewoodharry@welldev.io>

--- rest/src/main/java/mu/server/rest/config/RestClientConfig.java
+++ rest/src/main/java/mu/server/rest/config/RestClientConfig.java
@@ -1,8 +1,13 @@
 package mu.server.rest.config;
 
+import mu.server.service.exception.InvalidCallException;
+import mu.server.service.exception.JsonPlaceHolderException;
+import mu.server.service.exception.NotFoundException;
 import mu.server.service.service.http.JsonPlaceHolderService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.web.client.ApiVersionInserter;
 import org.springframework.web.client.support.RestClientHttpServiceGroupConfigurer;
@@ -18,6 +23,13 @@ public class RestClientConfig {
     public RestClientHttpServiceGroupConfigurer configurer() {
         return groups -> groups.filterByName("jsonplaceholder")
                 .forEachClient((_, restClient) -> restClient.baseUrl(URL)
+                        .defaultStatusHandler(HttpStatusCode::isError, (_, response) -> {
+                            switch (response.getStatusCode()) {
+                                case HttpStatus.NOT_FOUND -> throw new NotFoundException("Request Not Found");
+                                case HttpStatus.BAD_REQUEST -> throw new InvalidCallException("Bad Request");
+                                default -> throw new JsonPlaceHolderException("JsonPlace api error");
+                            }
+                        })
                         .apiVersionInserter(ApiVersionInserter.builder().useHeader("X-API-Version").build())
                         .defaultHeader("Content-Type", "application/json")
                         .defaultHeader("Accept", MediaType.APPLICATION_JSON_VALUE)
