28679b660401425065a9465b7694537ccea0fde6
Pranav <46899146+Bigorno12@users.noreply.github.com>
2026-05-14 16:27:31 +0400

    Feature/rate limiter (#216)
    * feat: Grafana, redis, ratelimt
    
    - Remove Grafana, OpenTelemetry config in spring
    - Update the docker compose file, add redis
    - Add rate limiter in the project
    
    * feat: Encryption
    - add encryption converter
    - encrypt sensitive field
    - encrypt email field
    
    ---------
    
    Co-authored-by: pranavseewoodharry <pranav.seewoodharry@welldev.io>

---
+++ rest/src/main/java/mu/server/rest/filter/RateLimitFilter.kt
@@ -0,0 +1,37 @@
+package mu.server.rest.filter
+
+import io.github.bucket4j.Bandwidth
+import io.github.bucket4j.Bucket
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import java.time.Duration
+
+@Component
+class RateLimitFilter : OncePerRequestFilter() {
+
+    private val bucket: Bucket = Bucket.builder()
+        .addLimit(
+            Bandwidth.builder()
+                .capacity(100)
+                .refillGreedy(100, Duration.ofMinutes(1))
+                .build()
+        )
+        .build()
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        if (bucket.tryConsume(1)) {
+            filterChain.doFilter(request, response)
+        } else {
+            response.status = HttpStatus.TOO_MANY_REQUESTS.value()
+            response.writer.write("Too many requests!")
+        }
+    }
+}
